6ed6520c1d9101d63335f8b8586a8b3af51bf28c
Paul Davis <paul.davis@ecc-solutions.com>
2026-07-03 18:33:12 +0000

    Add Razor Pages host to API project

---
+++ src/AngularDotNetLab.Api/Pages/Legacy/Customers.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AngularDotNetLab.Api.Pages.Legacy;
+
+public class CustomersModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
--- src/AngularDotNetLab.Api/Program.cs
+++ src/AngularDotNetLab.Api/Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddRazorPages();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -13,6 +14,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.MapRazorPages();
 
 var summaries = new[]
 {
